cc33351cc478d1f0e8608a20ec2f96fa06140719
Administrador CAA <ADM-CAA@caa.local>
2018-04-01 11:55:24 -0400

    initial commit

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace SSTI
             clase2 cl2 = new clase2();
             int m;
             Console.WriteLine("Practica 1");
-            Console.WriteLine("1 - Numero mayor, 2 - Inicio de Sesion, 3 - Numero par o Impar, 4 - Calculadora magica");
+            Console.WriteLine("1 - Numero mayor, 2 - Inicio de Sesion, 3 - Numero par o Impar, 4 - Calculadora");
             m = Convert.ToInt16(Console.ReadLine());
             switch (m)
             {
